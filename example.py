5587acb6a513f0cc4674f03c2ca871131e300117
Zdeněk Matěj <zdenek.matej@maxiv.lu.se>
2018-05-16 15:58:44 +0200

    Adjusting exmaple.py for the next excercise

--- example.py
+++ example.py
@@ -1,15 +1,3 @@
-def reverse_string(s):
-    """
-    Reverses order or characters in string s.
-    """
-    return s[::-1]
-
-
-def test_reverse_string():
-    assert reverse_string('foobar!') == '!raboof'
-    assert reverse_string('stressed') == 'desserts'
-
-
 def reverse_words(s):
     """
     Reverses order or words in string s.
@@ -23,3 +11,26 @@ def test_reverse_words():
     assert reverse_words('dogs hate cats') == 'cats hate dogs'
     assert reverse_words('dog eat dog') == 'dog eat dog'
     assert reverse_words('one two three four') == 'four three two one'
+
+
+def get_word_lengths(s):
+    """
+    Returns a list of integers representing
+    the word lengths in string s.
+    """
+    # uncomment next line in step 9
+#   return [len(word) for word in s.split()]
+    return None
+
+
+# uncomment this function in step 6
+#def test_get_word_lengths():
+#    text = "Three tomatoes are walking down the street"
+#    assert get_word_lengths(text) == [5, 8, 3, 7, 4, 3, 6]
+
+
+def obscure_function():
+    """
+    Example of a function that is never tested.
+    """
+    do_something_strange()
