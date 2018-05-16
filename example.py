3fc323e45c633aaddbf3c62a6758353294702698
Zdeněk Matěj <zdenek.matej@maxiv.lu.se>
2018-05-16 16:23:08 +0200

    Breaking example.py in Step 6

--- example.py
+++ example.py
@@ -24,9 +24,9 @@ def get_word_lengths(s):
 
 
 # uncomment this function in step 6
-#def test_get_word_lengths():
-#    text = "Three tomatoes are walking down the street"
-#    assert get_word_lengths(text) == [5, 8, 3, 7, 4, 3, 6]
+def test_get_word_lengths():
+    text = "Three tomatoes are walking down the street"
+    assert get_word_lengths(text) == [5, 8, 3, 7, 4, 3, 6]
 
 
 def obscure_function():
